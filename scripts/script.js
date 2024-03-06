e05cee7c66c5bfe24b5713651919b8b8b5137006
thomaswparks <67939699+thomaswparks@users.noreply.github.com>
2024-03-06 15:49:54 -0500

    Update script.js
    Added functionality for custom alert box.

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+let alertBoxContainer = document.querySelector('.alert__box__container');
+let viewAlertBtn1 = document.querySelector('.coming-soon-btn-1');
+let viewAlertBtn2 = document.querySelector('.coming-soon-btn-2');
+let DismissBtn = document.querySelector('.dismiss__btn');
+
+viewAlertBtn1.onclick = () =>{
+	alertBoxContainer.style.display = 'flex';
+}
+viewAlertBtn2.onclick = () =>{
+	alertBoxContainer.style.display = 'flex';
+}
+DismissBtn.onclick = () =>{
+	alertBoxContainer.style.display = 'none';
+}
