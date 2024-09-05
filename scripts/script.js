afa4b33bb308cdc66745b09ff1d869f62b941536
thomaswparks <thomaswparks@gmail.com>
2024-09-05 10:15:23 -0400

    removed remnants of javascript sidebar function

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,3 @@
-// function toggleMenu() {
-//     const menu = document.querySelector(".menu-links");
-//     const icon = document.querySelector(".sidebar-hamburger-icon");
-//     menu.classList.toggle("open")
-//     icon.classList.toggle("open")
-// }
-
 let alertBoxContainer = document.querySelector('.alert__box__container');
 let viewAlertBtn1 = document.querySelector('.coming-soon-btn-1');
 let viewAlertBtn2 = document.querySelector('.coming-soon-btn-2');
